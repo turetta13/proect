13b09e2b3028c94262241c6106c91999b7258b02
turetta13 <turetta13@gmail.com>
2019-05-13 22:33:31 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -6,21 +6,3 @@ Route::get('/contact', 'PageController@contact');
 Route::get('/prod', 'PageController@prod');
 Route::get('/single', 'PageController@single');
 
-//Route::get('/', function () {
-//    return view('index');
-//});
-//Route::get('/acc', function () {
-//        return view('account');
-//    });
-//Route::get('', function () {
-//    return view('checkout');
-//});
-//Route::get('/contact', function () {
-//    return view('contact');
-//});
-//Route::get('/prod', function () {
-//    return view('products');
-//});
-//Route::get('/single', function () {
-//    return view('single');
-//});
