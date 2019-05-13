78c874dda68e460a0db67be840cb84c15dab2365
turetta13 <turetta13@gmail.com>
2019-05-13 22:23:04 +0300

    fix

--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,26 @@
 <?php
+Route::get('/', 'PageController@index');
+Route::get('/acc', 'PageController@acc');
+Route::get('/check', 'PageController@check');
+Route::get('/contact', 'PageController@contact');
+Route::get('/prod', 'PageController@prod');
+Route::get('/single', 'PageController@single');
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('index');
-   });
-Route::get('/acc', function () {
-        return view('account');
-    });
-Route::get('/check', function () {
-    return view('checkout');
-});
-Route::get('/contact', function () {
-    return view('contact');
-});
-Route::get('/prod', function () {
-    return view('products');
-});
-Route::get('/single', function () {
-    return view('single');
-});
+//Route::get('/', function () {
+//    return view('index');
+//});
+//Route::get('/acc', function () {
+//        return view('account');
+//    });
+//Route::get('', function () {
+//    return view('checkout');
+//});
+//Route::get('/contact', function () {
+//    return view('contact');
+//});
+//Route::get('/prod', function () {
+//    return view('products');
+//});
+//Route::get('/single', function () {
+//    return view('single');
+//});
